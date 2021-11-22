35416a9c55ede394aa32ba813c0d5e85ea1e621a
cSora <somfaida@gmail.com>
2021-11-22 20:38:03 +0100

    fix: remove redundant implement statement

--- src/test/java/com/example/tw3/tests/LoginTest.java
+++ src/test/java/com/example/tw3/tests/LoginTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class LoginTest implements LoginLogOut {
+public class LoginTest {
     LoginTestsUtility loginMethods = new LoginTestsUtility();
 
     @AfterEach

8aae0c80d2d4566da23b9b95f6818eeea4b5576d
cSora <somfaida@gmail.com>
2021-11-22 20:30:37 +0100

    feat: create setters for element locating

--- src/test/java/com/example/tw3/pages/BrowseProjectsPage.java
+++ src/test/java/com/example/tw3/pages/BrowseProjectsPage.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class BrowseProjectsPage {
     public final String url = "https://jira-auto.codecool.metastage.net/secure/BrowseProjects.jspa";
+    String browseUrl;
     SelenideElement mainTestingProjectLink = $x("//a[contains(@original-title, 'Main Testing Project')]");
     SelenideElement projectTitle = $x("//a[contains(text(), 'Main Testing Project')]");
     SelenideElement projectKey = $x("//dd[contains(text(), 'MTP')]");
@@ -23,10 +24,18 @@ public class BrowseProjectsPage {
     }
 
     public void setProjectTitle(String projectTitle) {
-        this.projectTitle = $x("//a[contains(text(),"+projectTitle+" )]");
+        this.projectTitle = $x("//a[contains(text(),'"+projectTitle+"' )]");
     }
 
     public void setProjectKey(String key) {
-        this.projectKey = $x("//dd[contains(text(), "+key+")]");
+        this.projectKey = $x("//dd[contains(text(), '"+key+"')]");
+    }
+
+    public String getBrowseUrl() {
+        return browseUrl;
+    }
+
+    public void setBrowseUrl(String key) {
+        this.browseUrl = "https://jira-auto.codecool.metastage.net/projects/"+key+"/summary";
     }
 }

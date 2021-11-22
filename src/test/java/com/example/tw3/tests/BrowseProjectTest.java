1a62c6bead4cbaba514690158d25ebf384070011
cSora <somfaida@gmail.com>
2021-11-22 20:30:21 +0100

    feat: implement test for accessing given projects

--- src/test/java/com/example/tw3/tests/BrowseProjectTest.java
+++ src/test/java/com/example/tw3/tests/BrowseProjectTest.java
@@ -5,6 +5,8 @@ import com.example.tw3.utility.Login;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowseProjectTest {
@@ -21,5 +23,14 @@ public class BrowseProjectTest {
         browseProjectUtility.clickMainTestingProject();
         Assertions.assertTrue(browseProjectUtility.validateProjectKey());
         Assertions.assertTrue(browseProjectUtility.validateProjectName());
+    }
+
+    @ParameterizedTest(name = "Test browse {0}")
+    @MethodSource("com.example.tw3.utility.BrowseProjectUtility#provideProjectNamesAndKeys")
+    public void test(String projectName, String projectKey){
+        browseProjectUtility.setProjectUrl(projectKey);
+        Assertions.assertTrue(browseProjectUtility.validateProjectKey(projectKey));
+        Assertions.assertTrue(browseProjectUtility.validateProjectName(projectName));
+
     }
 }

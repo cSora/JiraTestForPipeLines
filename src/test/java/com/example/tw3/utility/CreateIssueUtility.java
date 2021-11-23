7d175ac8afa1df0f45a4ded12368174494fe0797
HuBori <hunyadyb2000@gmail.com>
2021-11-23 11:19:50 +0100

    feat: implement CreateIssueUtility

--- src/test/java/com/example/tw3/utility/CreateIssueUtility.java
+++ src/test/java/com/example/tw3/utility/CreateIssueUtility.java
@@ -3,16 +3,23 @@ package com.example.tw3.utility;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.example.tw3.pages.DashBoardPage;
+import com.example.tw3.pages.IssuePage;
+import com.example.tw3.pages.dropdowns.MoreOptionsDropDown;
 import com.example.tw3.pages.screens.CreateIssueScreen;
+import com.example.tw3.pages.screens.DeleteIssueScreen;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CreateIssueUtility {
-    DashBoardPage dashBoardPage = new DashBoardPage();
-    CreateIssueScreen createIssueScreen = new CreateIssueScreen();
+import static com.codeborne.selenide.Selenide.open;
 
-    WebDriverWait wait;
+public class CreateIssueUtility {
+    private DashBoardPage dashBoardPage = new DashBoardPage();
+    private CreateIssueScreen createIssueScreen = new CreateIssueScreen();
+    private IssuePage issuePage = new IssuePage();
+
+    private WebDriverWait wait;
+    private String issueUrl;
 
     public void openScreen() {
         dashBoardPage.getCreateBtn().click();
@@ -41,6 +48,7 @@ public class CreateIssueUtility {
             field.sendKeys(Keys.BACK_SPACE);
         }
         field.sendKeys(value);
+        field.sendKeys(Keys.ENTER);
     }
 
     public boolean validateInForm(String project, String issueType, String summary) {
@@ -54,5 +62,34 @@ public class CreateIssueUtility {
         wait = new WebDriverWait(WebDriverRunner.getWebDriver(), 5);
         wait.until(ExpectedConditions.visibilityOf(createIssueScreen.getIssueLink()));
         createIssueScreen.getIssueLink().click();
+        wait.until(ExpectedConditions.visibilityOf(issuePage.getDetailsSection()));
+        issueUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+    }
+
+    public boolean validateIssuePage(String project, String type, String summary) { // TODO: debug
+        if (!issuePage.getProjectNameValue().getText().equals(project)) { return false; }
+        if (!issuePage.getTypeValue().getText().equals(type)) { return false; }
+        if (!issuePage.getSummaryValue().getText().equals(summary)) { return false; }
+        // if (!issuePage.getReporterValue().getText().equals(System.getenv("user"))) { return false; }
+        return true;
+    }
+
+    public void deleteIssue() {
+        issuePage.getMoreBtn().click();
+        MoreOptionsDropDown more = new MoreOptionsDropDown();
+        while (!more.getDeleteOption().isDisplayed()) {
+            more.getMenu().sendKeys(Keys.DOWN);
+        }
+        more.getDeleteOption().click();
+        DeleteIssueScreen delete = new DeleteIssueScreen();
+        wait.until(ExpectedConditions.visibilityOf(delete.getDialog()));
+        wait.until(ExpectedConditions.visibilityOf(delete.getMessage()));
+        delete.getDeleteBtn().click();
+    }
+
+    public boolean validateDeleted() {
+        open(issueUrl);
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        return !currentUrl.equals(issueUrl);
     }
 }

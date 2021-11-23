17d2411392621d065ca854d2166072586659a607
cSora <somfaida@gmail.com>
2021-11-23 12:18:41 +0100

    feat: rename

--- src/test/java/com/example/tw3/pages/BrowseIssuePage.java
+++ src/test/java/com/example/tw3/pages/BrowseIssuePage.java
@@ -4,9 +4,18 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$x;
 
-public class BrowseIssue {
+public class BrowseIssuePage {
     public final String preSetIssueUrl = "https://jira-auto.codecool.metastage.net/browse/MTP-1188";
     String customIssueUrl = "https://jira-auto.codecool.metastage.net/browse/";
     SelenideElement issueSummary = $x("//h1[@id='summary-val']");
     SelenideElement issueKey = $x("//a[@id='key-val']");
+
+
+    public SelenideElement getIssueSummary() {
+        return issueSummary;
+    }
+
+    public SelenideElement getIssueKey() {
+        return issueKey;
+    }
 }

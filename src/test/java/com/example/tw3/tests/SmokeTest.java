adf5bcb7f7a197831df07b2624767a098c6ff63a
HuBori <hunyadyb2000@gmail.com>
2021-11-24 11:00:23 +0100

    feat: add more tests to SmokeTest

--- src/test/java/com/example/tw3/tests/SmokeTest.java
+++ src/test/java/com/example/tw3/tests/SmokeTest.java
@@ -1,14 +1,51 @@
 package com.example.tw3.tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SmokeTest {
     private LoginTest login = new LoginTest();
     private LogoutTest logout = new LogoutTest();
+    private BrowseIssueTest browseIssue = new BrowseIssueTest();
+    private BrowseProjectTest browseProject = new BrowseProjectTest();
+    private CreateIssueTest createIssue = new CreateIssueTest();
+    private EditIssueTest editIssue = new EditIssueTest();
+
+    @BeforeEach
+    public void setUp() {
+        logout.login();
+    }
 
     @Test
-    public void smoke() {
-        login.login();
+    public void logout() {
         logout.logout();
+        logout.login();
+        logout.multiTabLogout();
+    }
+
+    @Test
+    public void login(){
+        logout.logout();
+        login.login();
+    }
+
+    @Test
+    public void browseIssue() {
+        browseIssue.preSetIssueTest();
+    }
+
+    @Test
+    public void browseProject() {
+        browseProject.browseProject();
+    }
+
+    @Test
+    public void createIssue(){
+        createIssue.createIssue();
+    }
+
+    @Test
+    public void editIssue(){
+        editIssue.editTest();
     }
 }

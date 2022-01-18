f77262662fface029230fce32fb055e4150041b6
cSora <somfaida@gmail.com>
2022-01-18 12:28:52 +0100

    test: test commit for autobuild

--- src/test/java/com/example/tw3/utility/LoginLogOut.java
+++ src/test/java/com/example/tw3/utility/LoginLogOut.java
@@ -11,7 +11,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
